23f26ae34eef7a774cda32a006ec6b58e460cc0e
Ershov Evgeniy <ershov.ee@gmail.com>
2023-02-10 18:18:38 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import conveyorRoutes from './routes/conveyorRoutes.js';
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,17 +8,13 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Banking credit conveyor MVP1')
-})
+  res.send('Banking credit conveyor MVP1');
+});
 
-import conveyorRoutes from './routes/conveyorRoutes.js';
 
 app.use('/conveyor', conveyorRoutes);
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
 
-
-// POST: /conveyor/offers - расчёт возможных условий кредита. Request - LoanApplicationRequestDTO, response - List<LoanOfferDTO>
-// POST: /conveyor/calculation - валидация присланных данных + скоринг данных + полный расчет параметров кредита. Request - ScoringDataDTO, response CreditDTO.
